93ff3569532bad8694272a37102e3fdb795e58e2
PilarOnd <pilarondarzabanegas@gmail.com>
2025-03-14 11:35:31 -0400

    menu filtro, circulos

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -30,18 +30,32 @@
         <h5>CAMPAÑAS</h5>
         
         <div class="filter-section">
-            <div class="year-filter">
-                <span class="filter-option active">2025</span>
+            <!-- Skeleton loading para los filtros -->
+            <div id="loading-filters" class="loading">
+                <div class="skeleton filter"></div>
+                <div class="skeleton filter"></div>
+                <div class="skeleton filter"></div>
             </div>
-            
-            <div class="month-filter">
-                <span class="filter-option">Todo</span>
-                <span class="filter-option active">FEBRERO</span>
+
+            <div id="filters-content" style="display: none;">
+                <div class="year-filter">
+                    <span class="filter-option active">2025</span>
+                </div>
+                
+                <div class="month-filter">
+                    <span class="filter-option">Todo</span>
+                    <span class="filter-option active">FEBRERO</span>
+                </div>
             </div>
         </div>
 
         <div class="campaigns-list" id="campaignsList">
-            <!-- Las campañas se insertarán aquí -->
+            <!-- Skeleton loading para las campañas -->
+            <div id="loading-campaigns" class="loading">
+                <div class="skeleton title"></div>
+                <div class="skeleton"></div>
+                <div class="skeleton"></div>
+            </div>
         </div>
 
         <div class="download-section">
